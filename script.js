f810e9782f144f1eccd0d9eb4568743d47c0b068
PHYOE WAI AUNG <phyoewaiaung082@gmail.com>
2022-04-26 23:09:39 +0630

    Updating IF ELSE Conditions format in a single play round session

--- script.js
+++ script.js
@@ -9,61 +9,81 @@ function computerPlay() {
       return "scissors";
   }
 }
-
+console.log(computerPlay());
+var playerScore = 0;
+var computerScore = 0;
 /** a function that plays a single round of Rock Paper Scissors */
-function playRound(playerSelection,computerSelection) {
-    let player = playerSelection;
-    let computer = computerSelection;
-    console.log("You - " + player);
-    console.log("computer - " + computer);
-    if(player == computerSelection){
-        ++countPlayer;
-        ++countComputer;
+function playRound(playerSelection, computerSelection) {
+    console.log(playerSelection);
+    console.log(computerSelection);
+    if(playerSelection == computerSelection) {
+        ++playerScore;
+        ++computerScore;
         return "DRAW";
-    }else if(player == "rock" && computer == "paper") {
-        ++countComputer;
-        return "You Lose, Paper beats Rock!";
-    }else if(player == "rock" && computer == "scissors") {
-        ++countPlayer;
-        return "You Win, Rock beats scissors!";
-    }else if(player == "paper" && computer == "rock"){
-        ++countPlayer;
-        return "You Win, Paper beats Rock!";
-    }else if(player == "paper" && computer == "scissors"){
-        ++countComputer;
-        return "You Lose, Scissors beats paper!";
-    }else if(player == "scissors" && computer == "paper"){
-        ++countPlayer;
-        return "You Win, Scissors beat paper!";
-    }else if(player == "scissors" && computer == "rock"){
-        ++countComputer;
-        return "You Lose, Rock beats Scissors!";
     }
-}
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection,computerSelection));
-var countPlayer = 0;
-var countComputer = 0;
+    if(playerSelection == "rock"){
+        if(computerSelection == "paper") {
+            ++computerScore;
+            return "Computer Score - PAPER WINS";
+
+        } else {
+            ++playerScore;
+            return "Player Score - ROCK WINS";
+        }
+    }
+    if(playerSelection == "paper") {
+        if(computerSelection == "rock") {
+            ++playerScore;
+            return "Player Score - PAPER WINS";
+        } else if(computerSelection == "scissors"){
+            ++computerScore;
+            return "Computer Score - SCISSORS WINS";
+        }
+    }
+    if(playerSelection == "scissors") {
+        if(computerSelection == "rock") {
+            ++computerScore;
+            return "Computer Score - ROCK WINS";
+        } else if(computerSelection == "paper") {
+            ++playerScore;
+            return "Player Score - SCISSORS WINS";
+        }
+    }
+  }
+  
+// var countPlayer = 0;
+// var countComputer = 0;
 
 /** FIVE Rounds game function  */
 function game() {
     for (let i = 0; i < 5; i++) {
-        let playerChoice = prompt("ROCK PAPER SCISSORS?");
+        let playerChoice = prompt("ROCK PAPER SCISSORS?").toLowerCase();
         let computerChoice = computerPlay();
         console.log(playRound(playerChoice,computerChoice));
-    }  
+        console.log("player - " + playerScore);
+        console.log("computer - " +computerScore);
+}
+if(playerScore == computerScore) {
+    return "This match is a tie";
+} else if(playerScore > computerScore){
+    return "CONGRATS YOU WIN!";
+}
+else {
+    return "SORRY YOU LOSE!";
+}
 }
 
-/** Function that declare Winner */
-function winner() {
-    if(countPlayer > countComputer) {
-        return "CONGRATS TO PLAYER";
-    }
-    else {
-        return "SORRY TO PLAYER";
-    }
-}
+console.log(game());
 
-game();
-console.log(winner());
+// /** Function that declare Winner */
+// function winner() {
+//     if(countPlayer > countComputer) {
+//         return "CONGRATS TO PLAYER";
+//     }
+//     else {
+//         return "SORRY TO PLAYER";
+//     }
+// }
+
+// game();
+// console.log(winner());
